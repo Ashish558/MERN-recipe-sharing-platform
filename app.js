58d46f2d001e0b1fea73a094c3f7a9079b308589
Ashish558 <77892788+Ashish558@users.noreply.github.com>
2022-02-03 13:46:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ var cors = require('cors')
 require("dotenv").config()
 
 var app = express();
+const domain = 'https://desolate-mesa-23396.herokuapp.com'
 
 const corsOptions = {
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token', 'Authorization', "auth-token"],
   credentials: true,
   methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-  origin: 'http://localhost:3000',
+  origin: domain,
   preflightContinue: false,
 }
 
@@ -18,10 +19,8 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.static(__dirname + "/public"))
 
-const uri1 = "mongodb://ash:1234@cluster0-shard-00-00.dyw2u.mongodb.net:27017,cluster0-shard-00-01.dyw2u.mongodb.net:27017,cluster0-shard-00-02.dyw2u.mongodb.net:27017/Recipe?ssl=true&replicaSet=atlas-xnnv2d-shard-0&authSource=admin&retryWrites=true&w=majority"
 
-const uri = 'mongodb+srv://ash:1234@cluster0.dyw2u.mongodb.net/Recipe?retryWrites=true&w=majority'
-mongoose.connect(uri1, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("connected to db"))
   .catch((err) => console.log(err))
 
