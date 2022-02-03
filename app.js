1cca0cba62ef31356b0b071a97a7cea412805432
Ashish558 <77892788+Ashish558@users.noreply.github.com>
2022-02-03 14:42:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -43,6 +43,6 @@ app.get("*", (req, res) => {
 });
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 4000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
