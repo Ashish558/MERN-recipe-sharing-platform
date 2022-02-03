142de8ea8e6c43a8f3912cc970dbff710c2f2ec2
Ashish558 <77892788+Ashish558@users.noreply.github.com>
2022-02-03 14:06:37 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
 const Home = () => {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+
+  useEffect(() => {
+    console.log(isAuthenticated)
+    if (isAuthenticated) window.location='/posts'
+     return window.location='/login'
+
+  }, [])
 
   return (
     <div>
