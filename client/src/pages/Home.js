242e08332e2d88b9bc96246176fa38b72d998c87
Ashish558 <77892788+Ashish558@users.noreply.github.com>
2022-02-03 14:02:38 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,21 +1,6 @@
 import React from 'react'
 
 const Home = () => {
- 
-  // useEffect(() => {
-  //   var options = {
-  //     method: 'GET',
-  //     url: 'https://api.spoonacular.com/recipes/complexSearch?apiKey=e92dd5a8a49c4a13ac9d98633f671b21',
-  //     params: { query: 'chicken' }
-  //   };
-    
-  //   axios.request(options).then(function (response) {
-  //     console.log(response.data);
-  //   }).catch(function (error) {
-  //     console.error(error);
-  //   });
-
-  // }, [])
 
   return (
     <div>
