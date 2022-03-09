913f25300df763ae0999943365ec9dd24d8f2fb6
Ashish558 <77892788+Ashish558@users.noreply.github.com>
2022-03-09 20:03:37 +0530

    Update styles.js

--- client/src/components/posts/Filter/InputFields/styles.js
+++ client/src/components/posts/Filter/InputFields/styles.js
@@ -5,27 +5,24 @@ border: 0;
 outline: none;
 height: 28px;
 background: #e9e9e97a;
-border-radius: 0.3rem;
+border-radius: 0.3rem 0 0 0.3rem;
 padding: 0.2rem 0.5rem;
 flex: 1;
+min-width: 130px;
 `
 
 export const InputBox = styled.div`
 display: flex;
+justifyContent: flex-start;
 align-items: center;
 `
 
 export const Button = styled.button`
-margin-left: 1rem;
-padding: 0.42rem 0.5rem;
-border: 1px solid #b1b0b07a;
-background: transparent;
-border-radius: 0.3rem;
+padding: 0.45rem 0.5rem;
+border: 0px solid #b1b0b07a;
+background: #ea4c89d4;
+border-radius: 0 0.3rem 0.3rem 0;
 transition: 0.2s;
-
-&:hover {
-    border: 1px solid #e9e9e97a;
-    background: #e9e9e97a;
-  }
-}
+color: white;
+cursor: pointer;
 }`
