cd41e3877673d541205d2e3ba29d79c02629d8b2
Ashish558 <77892788+Ashish558@users.noreply.github.com>
2022-03-09 19:52:11 +0530

    Update CreatePost.js

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -56,7 +56,7 @@ export default function CreatePost(props) {
          }
       })
          .then(res => {
-            window.location = '/login'
+            window.location = '/'
          })
          .catch(err => {
             console.log(err)
